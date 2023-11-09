09aabaca98b31c278a4bb82b787ea74d28d18aae
Herman Kristiansen <gghh@live.no>
2023-11-09 15:30:21 +0100

    removed old code

--- app/example/[id]/page.js
+++ app/example/[id]/page.js
@@ -4,15 +4,11 @@ import { getExampleInput } from '../../../../lib/prisma/read/example.js'
 
  export default async function recTeam({params}) {
     const { example } = await getExample2ById(params.id)
-
-    const {Example} = await getExampleInput()
-
-    const input = Example.filter((input) => example.title === input.keyword)
     
     return (
         <main className='bg-white'>
             {/* passes down the example param to rec_example */}
-            <Example input={input} example = {example}></Example>
+            <Example example = {example}></Example>
         </main>
     )
   }
