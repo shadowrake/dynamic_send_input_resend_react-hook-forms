49f8c0168a96e148cd44bcba2457b22b07ba975b
Herman Kristiansen <gghh@live.no>
2023-11-09 15:31:27 +0100

    Update error.js

--- app/example/[id]/error.js
+++ app/example/[id]/error.js
@@ -26,7 +26,7 @@ export default function Error({ error, reset }) {
           <p className="mt-4 text-base  sm:mt-6">Sorry, we couldn’t find the page you’re looking for.</p>
           <div className="mt-10 flex justify-center">
             <a href="/" className="text-sm font-semibold leading-7 ">
-              <span aria-hidden="true">&larr;</span> Back to recruitment
+              <span aria-hidden="true">&larr;</span> Back home
             </a>
           </div>
         </div>
